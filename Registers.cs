58bb3960d5faa90aa16e97347ee5d05bbedfab07
a0zhar <96749372+a0zhar@users.noreply.github.com>
2024-04-16 00:35:47 +0200

    Commented Struct Members!
    Added a comment to every member of regs struct.
    Rewrote the comment-style for envxmm struct-members.

--- Registers.cs
+++ Registers.cs
@@ -1,53 +1,87 @@
 using System.Runtime.InteropServices;
 
 namespace libdebug {
-
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct regs {
+        /// <summary> General purpose register r15 </summary>
         public ulong r_r15;
+        /// <summary> General purpose register r14 </summary>
         public ulong r_r14;
+        /// <summary> General purpose register r13 </summary>
         public ulong r_r13;
+        /// <summary> General purpose register r12 </summary>
         public ulong r_r12;
+        /// <summary> General purpose register r11 </summary>
         public ulong r_r11;
+        /// <summary> General purpose register r10 </summary>
         public ulong r_r10;
+        /// <summary> General purpose register r9 </summary>
         public ulong r_r9;
+        /// <summary> General purpose register r8 </summary>
         public ulong r_r8;
+        /// <summary> Destination index register </summary>
         public ulong r_rdi;
+        /// <summary> Source index register </summary>
         public ulong r_rsi;
+        /// <summary> Base pointer register </summary>
         public ulong r_rbp;
+        /// <summary> Base index register </summary>
         public ulong r_rbx;
+        /// <summary> Data register </summary>
         public ulong r_rdx;
+        /// <summary> Counter register </summary>
         public ulong r_rcx;
+        /// <summary> Accumulator register </summary>
         public ulong r_rax;
+        /// <summary> Trap number </summary>
         public uint r_trapno;
+        /// <summary> Segment register FS </summary>
         public ushort r_fs;
+        /// <summary> Segment register GS </summary>
         public ushort r_gs;
+        /// <summary> Error number </summary>
         public uint r_err;
+        /// <summary> Extra segment register </summary>
         public ushort r_es;
+        /// <summary> Data segment register </summary>
         public ushort r_ds;
+        /// <summary> Instruction pointer register </summary>
         public ulong r_rip;
+        /// <summary> Code segment register </summary>
         public ulong r_cs;
+        /// <summary> RFLAGS register </summary>
         public ulong r_rflags;
+        /// <summary> Stack pointer register </summary>
         public ulong r_rsp;
+        /// <summary> Stack segment register </summary>
         public ulong r_ss;
-    }
+    };
+
 
     [StructLayout(LayoutKind.Sequential)]
     public struct envxmm {
-        public ushort en_cw; /* control word (16bits) */
-        public ushort en_sw; /* status word (16bits) */
-        public byte en_tw; /* tag word (8bits) */
+        /// <summary> Control word (16bits) </summary>
+        public ushort en_cw; 
+        /// <summary> Status word (16bits) </summary>
+        public ushort en_sw;
+        /// <summary> Tag word (8bits) </summary>
+        public byte en_tw;
+        /// <summary> TODO: Comment this Member </summary>
         public byte en_zero;
-        public ushort en_opcode; /* opcode last executed (11 bits ) */
-        public ulong en_rip; /* floating point instruction pointer */
-        public ulong en_rdp; /* floating operand pointer */
-        public uint en_mxcsr; /* SSE sontorol/status register */
-        public uint en_mxcsr_mask; /* valid bits in mxcsr */
+        /// <summary> Opcode last executed (11 bits) </summary>
+        public ushort en_opcode;
+        /// <summary> Floating point instruction pointer </summary>
+        public ulong en_rip;
+        /// <summary> Floating operand pointer </summary>
+        public ulong en_rdp;
+        /// <summary> SSE sontorol/status register </summary>
+        public uint en_mxcsr;
+        /// <summary> Valid bits in mxcsr </summary>
+        public uint en_mxcsr_mask;
     }
 
     [StructLayout(LayoutKind.Sequential)]
     public struct acc {
-
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
         public byte[] fp_bytes;
 
@@ -57,7 +91,6 @@ namespace libdebug {
 
     [StructLayout(LayoutKind.Sequential)]
     public struct xmmacc {
-
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
         public byte[] xmm_bytes;
     }
