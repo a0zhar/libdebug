7290efb968a088080059b3d9839e5ca74fa116ed
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <PfizerJunkie@protonmail.com>
2024-04-21 16:38:14 +0200

    New Function Added!
    I made a DebugPrintWarning() function, which is defined inside of the file where DebugPrint() is located. This new function can be used to log warnings!

--- DBG.Printing.cs
+++ DBG.Printing.cs
@@ -8,8 +8,14 @@ namespace libdebug {
         /// <summary>
         /// Print's message in following format: From [Name of the Function from where this is used in]() - [Message Here]
         /// </summary>
-        public static void DebugPrint(string message, [CallerMemberName] string callerName = "") {
-            Console.WriteLine($"From {callerName}() - {message}");
-        }
+        public static void DebugPrint(string message, [CallerMemberName] string callerName = "") 
+            => Console.WriteLine($"From {callerName}() - {message}");
+        
+        /// <summary>
+        /// Print's warning in following format: WARNING! In [Function Name]() - [Message here]
+        /// </summary>
+        public static void DebugPrintWarning(string message, [CallerMemberName] string callerName = "")
+            => Console.WriteLine($"WARNING! In {callerName}() - {message}");
+        
     }
 }
