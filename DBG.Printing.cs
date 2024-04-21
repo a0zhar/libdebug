1ad7bcd93339b9645252b26e68eebc4671cb9e93
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <PfizerJunkie@protonmail.com>
2024-04-21 14:24:57 +0200

    New Release, i guess!

--- DBG.Printing.cs
+++ DBG.Printing.cs
@@ -1,12 +1,15 @@
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
+
 namespace libdebug {
+
     public partial class PS4DBG {
 
         /// <summary>
         /// Print's message in following format: From [Name of the Function from where this is used in]() - [Message Here]
         /// </summary>
-        public static void DebugPrint(string message, [CallerMemberName] string callerName = "")
-            => Console.WriteLine($"From {callerName}() - {message}");
+        public static void DebugPrint(string message, [CallerMemberName] string callerName = "") {
+            Console.WriteLine($"From {callerName}() - {message}");
+        }
     }
 }
