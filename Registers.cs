c8b8d4fc6abdd0501679c9d6826109bd4e17cd48
Discord: a0zhar#9539 <96749372+a0zhar@users.noreply.github.com>
2024-04-26 12:00:22 +0200

    Update Registers.cs
    All structs and enums commented, including every member.

--- Registers.cs
+++ Registers.cs
@@ -1,115 +1,321 @@
+
 using System.Runtime.InteropServices;
 
-namespace libdebug {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct regs {
-        public ulong r_r15;
-        public ulong r_r14;
-        public ulong r_r13;
-        public ulong r_r12;
-        public ulong r_r11;
-        public ulong r_r10;
-        public ulong r_r9;
-        public ulong r_r8;
-        public ulong r_rdi;
-        public ulong r_rsi;
-        public ulong r_rbp;
-        public ulong r_rbx;
-        public ulong r_rdx;
-        public ulong r_rcx;
-        public ulong r_rax;
-        public uint r_trapno;
-        public ushort r_fs;
-        public ushort r_gs;
-        public uint r_err;
-        public ushort r_es;
-        public ushort r_ds;
-        public ulong r_rip;
-        public ulong r_cs;
-        public ulong r_rflags;
-        public ulong r_rsp;
-        public ulong r_ss;
-    }
+/// <summary>
+/// Structure representing the CPU registers
+/// </summary>
+[StructLayout(LayoutKindSequential, Pack = 1)]
+public struct regs {
+    /// <summary>
+    /// General purpose register R15
+    /// </summary>
+    public ulong r_r15;
+    /// <summary>
+    /// General purpose register R14
+    /// </summary>
+    public ulong r_r14;
+    /// <summary>
+    /// General purpose register R13
+    /// </summary>
+    public ulong r_r13;
+    /// <summary>
+    /// General purpose register R12
+    /// </summary>
+    public ulong r_r12;
+    /// <summary>
+    /// General purpose register R11
+    /// </summary>
+    public ulong r_r11;
+    /// <summary>
+    /// General purpose register R10
+    /// </summary>
+    public ulong r_r10;
+    /// <summary>
+    /// General purpose register R9
+    /// </summary>
+    public ulong r_r9;
+    /// <summary>
+    /// General purpose register R8
+    /// </summary>
+    public ulong r_r8;
+    /// <summary>
+    /// Destination index register
+    /// </summary>
+    public ulong r_rdi;
+    /// <summary>
+    /// Source index register
+    /// </summary>
+    public ulong r_rsi;
+    /// <summary>
+    /// Base pointer register
+    /// </summary>
+    public ulong r_rbp;
+    /// <summary>
+    /// Base register
+    /// </summary>
+    public ulong r_rbx;
+    /// <summary>
+    /// Data register
+    /// </summary>
+    public ulong r_rdx;
+    /// <summary>
+    /// Count register
+    /// </summary>
+    public ulong r_rcx;
+    /// <summary>
+    /// Accumulator register
+    /// </summary>
+    public ulong r_rax;
+    /// <summary>
+    /// Trap number
+    /// </summary>
+    public uint r_trapno;
+    /// <summary>
+    /// Segment selector for FS
+    /// </summary>
+    public ushort r_fs;
+    /// <summary>
+    /// Segment selector for GS
+    /// </summary>
+    public ushort r_gs;
+    /// <summary>
+    /// Error code
+    /// </summary>
+    public uint r_err;
+    /// <summary>
+    /// Segment selector for ES
+    /// </summary>
+    public ushort r_es;
+    /// <summary>
+    /// Segment selector for DS
+    /// </summary>
+    public ushort r_ds;
+    /// <summary>
+    /// Instruction pointer
+    /// </summary>
+    public ulong r_rip;
+    /// <summary>
+    /// Code segment selector
+    /// </summary>
+    public ulong r_cs;
+    /// <summary>
+    /// Flags register
+    /// </summary>
+    public ulong r_rflags;
+    /// <summary>
+    /// Stack pointer
+    /// </summary>
+    public ulong r_rsp;
+    /// <summary>
+    /// Stack segment selector
+    /// </summary>
+    public ulong r_ss;
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct envxmm {
-        public ushort en_cw; /* control word (16bits) */
-        public ushort en_sw; /* status word (16bits) */
-        public byte en_tw; /* tag word (8bits) */
-        public byte en_zero;
-        public ushort en_opcode; /* opcode last executed (11 bits ) */
-        public ulong en_rip; /* floating point instruction pointer */
-        public ulong en_rdp; /* floating operand pointer */
-        public uint en_mxcsr; /* SSE sontorol/status register */
-        public uint en_mxcsr_mask; /* valid bits in mxcsr */
-    }
+/// <summary>
+/// Structure representing the floating-point environment and state
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct envxmm {
+    /// <summary>
+    /// Control word
+    /// </summary>
+    public ushort en_cw;
+    /// <summary>
+    /// Status word
+    /// </summary>
+    public ushort en_sw;
+    /// <summary>
+    /// Tag word
+    /// </summary>
+    public byte en_tw;
+    /// <summary>
+    /// Reserved
+    /// </summary>
+    public byte en_zero;
+    /// <summary>
+    /// Opcode last executed
+    /// </summary>
+    public ushort en_opcode;
+    /// <summary>
+    /// Floating-point instruction pointer
+    /// </summary>
+    public ulong en_rip;
+    /// <summary>
+    /// Floating-point operand pointer
+    /// </summary>
+    public ulong en_rdp;
+    /// <summary>
+    /// SSE control/status register
+    /// </summary>
+    public uint en_mxcsr;
+    /// <summary>
+    /// Valid bits in MXCSR
+    /// </summary>
+    public uint en_mxcsr_mask;
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct acc {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
-        public byte[] fp_bytes;
+/// <summary>
+/// Structure representing the floating-point accumulator
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct acc {
+    /// <summary>
+    /// Floating-point bytes
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 10)]
+    public byte[] fp_bytes;
+    private byte[] fp_pad; // Padding
+}
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
-        private byte[] fp_pad;
-    }
+/// <summary>
+/// Structure representing the XMM floating-point accumulator
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct xmmacc {
+    /// <summary>
+    /// XMM bytes
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 16)]
+    public byte[] xmm_bytes;
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct xmmacc {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public byte[] xmm_bytes;
-    }
+/// <summary>
+/// Structure representing the YMM floating-point accumulator
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct ymmacc {
+    /// <summary>
+    /// YMM bytes
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 16)]
+    public byte[] ymm_bytes;
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct ymmacc {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public byte[] ymm_bytes;
-    }
+/// <summary>
+/// Structure representing the header of extended CPU state
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct xstate_hdr {
+    /// <summary>
+    /// Extended state bitvector
+    /// </summary>
+    public ulong xstate_bv;
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 16)]
+    private byte[] xstate_rsrv0; // Reserved
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 40)]
+    private byte[] xstate_rsrv; // Reserved
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct xstate_hdr {
-        public ulong xstate_bv;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        private byte[] xstate_rsrv0;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 40)]
-        private byte[] xstate_rsrv;
-    }
+/// <summary>
+/// Structure representing the saved FPU extended state
+/// </summary>
+[StructLayout(LayoutKindSequential)]
+public struct savefpu_xstate {
+    /// <summary>
+    /// XSTATE header
+    /// </summary>
+    public xstate_hdr sx_hd;
+    /// <summary>
+    /// YMM floating-point accumulators
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 16)]
+    public ymmacc[] sx_ymm;
+}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct savefpu_xstate {
-        public xstate_hdr sx_hd;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public ymmacc[] sx_ymm;
-    }
+/// <summary>
+/// Structure representing the FPU registers
+/// </summary>
+[StructLayout(LayoutKindSequential, Pack = 64)]
+public struct fpregs {
+    /// <summary>
+    /// Floating-point environment
+    /// </summary>
+    public envxmm svn_env;
+    /// <summary>
+    /// Floating-point accumulators
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 8)]
+    public acc[] sv_fp;
+    /// <summary>
+    /// XMM floating-point accumulators
+    /// </summary>
+    [MarshalAs(UnmanagedTypeByValArray, SizeConst = 16)]
+    public xmmacc[] sv_xmm;
+    private byte[] sv_pad; // Padding
+    /// <summary>
+    /// Saved FPU extended state
+    /// </summary>
+    public savefpu_xstate sv_xstate;
+}
 
-    [StructLayout(LayoutKind.Sequential, Pack = 64)]
-    public struct fpregs {
-        public envxmm svn_env;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
-        public acc[] sv_fp;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public xmmacc[] sv_xmm;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 96)]
-        private byte[] sv_pad;
-        public savefpu_xstate sv_xstate;
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct dbregs {
-        public ulong dr0;
-        public ulong dr1;
-        public ulong dr2;
-        public ulong dr3;
-        public ulong dr4;
-        public ulong dr5;
-        public ulong dr6;
-        public ulong dr7;
-        public ulong dr8;
-        public ulong dr9;
-        public ulong dr10;
-        public ulong dr11;
-        public ulong dr12;
-        public ulong dr13;
-        public ulong dr14;
-        public ulong dr15;
-    }
+/// <summary>
+/// Structure representing the debug registers
+/// </summary>
+[StructLayout(LayoutKindSequential, Pack = 1)]
+public struct dbregs {
+    /// <summary>
+    /// Debug register 0
+    /// </summary>
+    public ulong dr0;
+    /// <summary>
+    /// Debug register 1
+    /// </summary>
+    public ulong dr1;
+    /// <summary>
+    /// Debug register 2
+    /// </summary>
+    public ulong dr2;
+    /// <summary>
+    /// Debug register 3
+    /// </summary>
+    public ulong dr3;
+    /// <summary>
+    /// Debug register 4
+    /// </summary>
+    public ulong dr4;
+    /// <summary>
+    /// Debug register 5
+    /// </summary>
+    public ulong dr5;
+    /// <summary>
+    /// Debug register 6
+    /// </summary>
+    public ulong dr6;
+    /// <summary>
+    /// Debug register 7
+    /// </summary>
+    public ulong dr7;
+    /// <summary>
+    /// Debug register 8
+    /// </summary>
+    public ulong dr8;
+    /// <summary>
+    /// Debug register 9
+    /// </summary>
+    public ulong dr9;
+    /// <summary>
+    /// Debug register 10
+    /// </summary>
+    public ulong dr10;
+    /// <summary>
+    /// Debug register 11
+    /// </summary>
+    public ulong dr11;
+    /// <summary>
+    /// Debug register 12
+    /// </summary>
+    public ulong dr12;
+    /// <summary>
+    /// Debug register 13
+    /// </summary>
+    public ulong dr13;
+    /// <summary>
+    /// Debug register 14
+    /// </summary>
+    public ulong dr14;
+    /// <summary>
+    /// Debug register 15
+    /// </summary>
+    public ulong dr15;
 }
